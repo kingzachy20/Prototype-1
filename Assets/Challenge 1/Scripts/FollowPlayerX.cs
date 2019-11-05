bd59e28b6b63fe4ea996ca1c7ce6885216c58214
Zachariah Reeves <56128534+kingzachy20@users.noreply.github.com>
2019-11-05 10:10:31 -0800

    fixed all the bugs

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject plane;
-    private Vector3 offset;
+    public GameObject Player;
+    private Vector3 offset = new Vector3(70,12,-1);
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.position = Player.transform.position + offset;
     }
 }
